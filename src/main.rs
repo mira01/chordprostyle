70ea7aa4aa2347f906b9340568044f2c76cf91a4
Miroslav Čech <miroslav.cech@gooddata.com>
2018-11-24 16:43:26 +0100

    read file

---
+++ src/main.rs
@@ -0,0 +1,20 @@
+extern crate chordprostyle;
+use chordprostyle::SongPart;
+
+use std::io::Read;
+use std::env;
+use std::fs::File;
+
+fn main(){
+    let args: Vec<String> = env::args().collect();
+    let mut f = File::open(&args[1]).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents);
+    
+    println!("I got {:?} args: {:?}", args.len()-1, &args[1..]);
+    println!("contents {}", contents);
+
+    for ch in contents.chars(){
+        println!("ch: {:?}", ch);
+    }
+}
