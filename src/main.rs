bdf8f83d1bb5e3bc446e40454ee61a9a3549418e
Miroslav Čech <miroslav.cech@gooddata.com>
2019-08-03 16:46:04 +0200

    use PdfFormatter in main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 extern crate chordprostyle;
 use chordprostyle::lex;
 use chordprostyle::HtmlFormatter;
+use chordprostyle::PdfFormatter;
 
 use std::io::Read;
 use std::env;
 use std::fs::File;
+
+extern crate printpdf;
+use printpdf::*;
+use std::io::BufWriter;
+
 
 fn main(){
     let args: Vec<String> = env::args().collect();
@@ -14,11 +20,9 @@ fn main(){
 
     let chars = contents.chars();
     let result = lex(chars);
+    //let formater = PdfFormatter::new(result);
     let formater = HtmlFormatter::new(result, "");
     let res = formater.format();
-    println!("{:?}", res);
-    //for a in result{
-    //    println!("{:?}", a);
-    //}
+    println!("{}", res);
 
 }
