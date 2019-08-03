fd6aab6f619093add6b13d89925d5066aa537758
Miroslav Čech <miroslav.cech@gooddata.com>
2019-08-03 16:59:04 +0200

    move HtmlFormatter within the file

--- src/lib.rs
+++ src/lib.rs
@@ -166,6 +166,55 @@ pub struct Line{
     song_parts: Vec<SongPart>,
 }
 
+pub struct HtmlFormatter<'a>{
+    lexer: Lexer<'a>,
+    stylesheet: &'a str, //TODO: fix injection
+}
+
+impl<'a> HtmlFormatter<'a>{
+    pub fn new(lexer: Lexer<'a>, stylesheet: &'a str) -> HtmlFormatter<'a>{
+        HtmlFormatter{
+            lexer: lexer,
+            stylesheet: stylesheet,
+        }
+    }
+    pub fn format(self) -> String{
+        let mut output = String::new();
+        output.push_str(&String::from(format!("<div class='song'>")));
+        for part in self.lexer{
+            match part{
+                SongPart::Text(text) => {
+                    output.push_str(&format!("{}", &text));
+                },
+                SongPart::Directive(DirectiveType::Title(value))=> {
+                    output.push_str(&format!("<h2>{}</h2>", &value));
+                },
+                SongPart::Directive(DirectiveType::ChorusStart)=> {
+                    output.push_str(&format!("<div class='ref'>"));
+                },
+                SongPart::Directive(DirectiveType::ChorusEnd)=> {
+                    output.push_str(&format!("</div>"));
+                },
+                SongPart::Directive(DirectiveType::Comment(value))=> {
+                    output.push_str(&format!("<span class='comment'>{}</span>", value));
+                },
+                SongPart::Directive(value) => {
+                    output.push_str(&String::from(format!("<div class='directive {:?}'>{:?}</div>", value, value)));
+                },
+                SongPart::Chord(text) => {
+                    output.push_str(&format!("<span class='chord'><strong class='chord'>{}</span></strong>", text));
+                },
+                SongPart::NewLine =>{
+                    output.push_str(&String::from("<br/>"));
+                },
+                _ => (),
+            }
+        }
+        output.push_str(&String::from("</div>"));
+        output
+    }
+}
+
 pub struct PdfFormatter<'a>{
     lexer: Lexer<'a>
 }
@@ -220,55 +269,6 @@ impl<'a> PdfFormatter<'a>{
     //    println!("{:?}", a);
     //}
 
-    }
-}
-
-pub struct HtmlFormatter<'a>{
-    lexer: Lexer<'a>,
-    stylesheet: &'a str, //TODO: fix injection
-}
-
-impl<'a> HtmlFormatter<'a>{
-    pub fn new(lexer: Lexer<'a>, stylesheet: &'a str) -> HtmlFormatter<'a>{
-        HtmlFormatter{
-            lexer: lexer,
-            stylesheet: stylesheet,
-        }
-    }
-    pub fn format(self) -> String{
-        let mut output = String::new();
-        output.push_str(&String::from(format!("<html><head><link rel='stylesheet' href='{}'></head><body>", self.stylesheet)));
-        for part in self.lexer{
-            match part{
-                SongPart::Text(text) => {
-                    output.push_str(&format!("{}", &text));
-                },
-                SongPart::Directive(DirectiveType::Title(value))=> {
-                    output.push_str(&format!("<h2>{}</h2>", &value));
-                },
-                SongPart::Directive(DirectiveType::ChorusStart)=> {
-                    output.push_str(&format!("<div class='ref'>"));
-                },
-                SongPart::Directive(DirectiveType::ChorusEnd)=> {
-                    output.push_str(&format!("</div>"));
-                },
-                SongPart::Directive(DirectiveType::Comment(value))=> {
-                    output.push_str(&format!("<span class='comment'>{}</span>", value));
-                },
-                SongPart::Directive(value) => {
-                    output.push_str(&String::from(format!("<div class='directive {:?}'>{:?}</div>", value, value)));
-                },
-                SongPart::Chord(text) => {
-                    output.push_str(&format!("<span class='chord'><strong class='chord'>{}</span></strong>", text));
-                },
-                SongPart::NewLine =>{
-                    output.push_str(&String::from("<br/>"));
-                },
-                _ => (),
-            }
-        }
-        output.push_str(&String::from("</body></html>"));
-        output
     }
 }
 
