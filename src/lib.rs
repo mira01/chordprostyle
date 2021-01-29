9626e38764615b3b2fa29ca3084f940bf8ab503a
Miroslav Čech <miroslav.cech@gooddata.com>
2021-01-29 15:35:40 +0100

    Move processing files into lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -3,3 +3,49 @@ pub mod parser;
 pub mod formatters;
 pub mod model;
 pub mod tri_parser;
+
+use std::io::Read;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
+use formatters::parse_formatter::ParseFormatter;
+
+pub fn process_files<T>(paths: T) where T: Iterator<Item=String>{
+    println!("<html><head><link rel='stylesheet' href='styl5.css'><meta charset='utf-8'></head><body>");
+    for (i, path) in paths.enumerate(){
+        match process_file(&path) {
+            Some(song) =>{
+                let formater = ParseFormatter::new(song);
+                let res = formater.format(&(i+1).to_string());
+                println!("{}", res);
+            },
+            None =>{
+                eprintln!("song {} error", path);
+            }
+        }
+    }
+    println!("</body></html>");
+}
+
+fn process_file(path: &String) -> Option<model::Song>{ //better be Result
+    let mut f = File::open(path).unwrap();
+    let mut contents = String::new();
+    f.read_to_string(&mut contents);
+
+    let chars = contents.chars();
+    let lexresult = lexer::lex(chars);
+    let lex_only = false;
+
+    if lex_only {
+        for token in lexresult{
+            println!("{:?}", token);
+        }
+        None
+    }else{
+        let mut parser = tri_parser::parse(lexresult);
+        let song = parser.parse();
+
+        Some(song)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,9 @@
-extern crate chordprostyle;
-use chordprostyle::lexer::lex;
-use chordprostyle::tri_parser::parse;
-use chordprostyle::formatters::parse_formatter::ParseFormatter;
-
+use chordprostyle as lib;
 use clap::{Arg, App};
 
 use std::io::Read;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::env;
 use std::fs::File;
 
 fn main(){
@@ -33,65 +28,6 @@ fn main(){
     } else {
         panic!("no files given")
     }
-    for i in iter {
-        println!("i: {:?}", i);
-    }
 
-
-}
-
-fn doit(){
-    let mut args = env::args().skip(1);
-
-    let switch = args.next().unwrap();
-    let paths : Vec<String> = if switch == "-l"{
-        let f = File::open(args.next().unwrap());
-        let mut file = BufReader::new(f.unwrap());
-        file.lines().map(|l|{l.unwrap().to_string()}).collect()
-    }
-    else if switch == "-f"{
-        args.map(|l| {l.to_string()}).collect()
-    }
-    else {
-        eprintln!("invocation: -l file_with_pathts | -f files");
-        panic!();
-    };
-
-
-    println!("<html><head><link rel='stylesheet' href='styl5.css'><meta charset='utf-8'></head><body>");
-    for (i, path) in paths.iter().enumerate(){
-        match process_file(&path) {
-            Some(song) =>{
-                let formater = ParseFormatter::new(song);
-                let res = formater.format(&(i+1).to_string());
-                println!("{}", res);
-            },
-            None =>{
-                eprintln!("song {} error", path);
-            }
-        }
-    }
-    println!("</body></html>");
-}
-
-fn process_file(path: &String) -> Option<chordprostyle::model::Song>{ //better be Result
-    let mut f = File::open(path).unwrap();
-    let mut contents = String::new();
-    f.read_to_string(&mut contents);
-
-    let chars = contents.chars();
-    let lexresult = lex(chars);
-    let lex_only = false;
-
-    if lex_only {
-        for token in lexresult{
-            println!("{:?}", token);
-        }
-        None
-    }else{
-        let mut parser = parse(lexresult);
-        let song = parser.parse();
-
-        Some(song)
-    }
+    lib::process_files(iter);
 }
