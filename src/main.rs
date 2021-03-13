1754ca09fab2a754217f10e29e8ac6d570345407
Miroslav Čech <miracech@email.cz>
2021-03-13 18:44:30 +0100

    Move getting source files to separate function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use chordprostyle as lib;
 use lib::formatters::TeraFormatter;
 use lib::tri_parser::TriParser;
-use clap::{Arg, App};
+use clap::{Arg, App, ArgMatches};
 
 use std::io::BufReader;
 use std::io::BufRead;
@@ -20,21 +20,7 @@ fn main(){
         )
         .get_matches();
 
-    let iter: Box<dyn Iterator<Item=String>>;
-    if let Some(files) = args.values_of("files"){
-        iter = Box::new(
-            files.map(|s| s.to_owned())
-        );
-    } else if let Some(path) = args.value_of("file with paths"){
-        let f = BufReader::new(File::open(path).expect("Could not open file with paths"));
-        iter = Box::new(
-            f.lines()
-             .map(|l| l.unwrap())
-        );
-    } else {
-        // TODO: read input from stdin
-        panic!("no files given")
-    }
+    let iter = source_files(&args);
 
     let mut parser = TriParser::new();
     let formatter = TeraFormatter::default(); //TeraFormatter::new();
@@ -42,4 +28,24 @@ fn main(){
         eprintln!("{:?}", errors);
         std::process::exit(1);
     }
+}
+
+fn source_files<'a>(args: &'a ArgMatches) -> Box<dyn Iterator<Item=String> +'a>{
+
+    let iter: Box<dyn Iterator<Item=String>>;
+    if let Some(files) = args.values_of("files"){
+        iter = Box::new(
+            files.map(|s| s.to_owned())
+            );
+    } else if let Some(path) = args.value_of("file with paths"){
+        let f = BufReader::new(File::open(path).expect("Could not open file with paths"));
+        iter = Box::new(
+            f.lines()
+            .map(|l| l.unwrap())
+            );
+    } else {
+        // TODO: read input from stdin
+        panic!("no files given")
+    }
+    iter
 }
