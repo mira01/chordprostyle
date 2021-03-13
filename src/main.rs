221d516ea9481a53b0d255a1f017d4499d02f141
Miroslav Čech <miracech@email.cz>
2021-03-13 23:20:17 +0100

    Make possible to pass custom templates

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,72 @@
 use chordprostyle as lib;
 use lib::formatters::TeraFormatter;
 use lib::tri_parser::TriParser;
+use lib::Formatter;
 use clap::{Arg, App, ArgMatches};
 
 use std::io::BufReader;
-use std::io::BufRead;
-use std::fs::File;
+use std::io::{BufRead, Read};
+use std::io;
+use std::fs::{self, File};
 
 fn main(){
     let args = App::new("ChordproStyle")
         .arg(Arg::with_name("files")
-            .takes_value(true)
-            .multiple(true)
-        )
+             .takes_value(true)
+             .multiple(true)
+            )
         .arg(Arg::with_name("file with paths")
-            .short("l")
-            .takes_value(true)
-            .conflicts_with("files")
-        )
+             .short("l")
+             .takes_value(true)
+             .conflicts_with("files")
+            )
+        .arg(Arg::with_name("template")
+             .short("t")
+             .long("template")
+             .takes_value(true)
+            )
         .get_matches();
 
     let iter = source_files(&args);
+    
+    let mut template_storage = String::new();
+    let formatter = formatter(&mut template_storage, &args).unwrap();
 
     let mut parser = TriParser::new();
-    let formatter = TeraFormatter::default(); //TeraFormatter::new();
-    if let Err(errors) = lib::process_files(iter, &mut parser, formatter){
+    if let Err(errors) = lib::process_files(iter.unwrap(), &mut parser, formatter){
         eprintln!("{:?}", errors);
         std::process::exit(1);
     }
 }
 
-fn source_files<'a>(args: &'a ArgMatches) -> Box<dyn Iterator<Item=String> +'a>{
+fn formatter<'a>(mut template_storage: &'a mut String, args: &'a ArgMatches) -> io::Result<TeraFormatter<'a>>{
+    match args.value_of("template"){
+        Some(template_file) => {
+            let mut f = BufReader::new(File::open(template_file)?);
+            f.read_to_string(&mut template_storage);
+            Ok(TeraFormatter::new(template_storage))
+        },
+        None => {
+            Ok(TeraFormatter::default())
+        }
+    }
+}
 
-    let iter: Box<dyn Iterator<Item=String>>;
+fn source_files<'a>(args: &'a ArgMatches) -> io::Result<Box<dyn Iterator<Item=String> +'a>>{
+    let iter: io::Result<Box<dyn Iterator<Item=String>>>;
     if let Some(files) = args.values_of("files"){
-        iter = Box::new(
+        iter = Ok(Box::new(
             files.map(|s| s.to_owned())
-            );
+            ));
     } else if let Some(path) = args.value_of("file with paths"){
-        let f = BufReader::new(File::open(path).expect("Could not open file with paths"));
-        iter = Box::new(
+        let f = BufReader::new(File::open(path)?);
+        iter = Ok(Box::new(
             f.lines()
             .map(|l| l.unwrap())
-            );
+            ));
     } else {
         // TODO: read input from stdin
-        panic!("no files given")
+        iter = Err(io::Error::new(io::ErrorKind::Other, "no files given"));
     }
     iter
 }
